3e3ab2968c6afad2e23f7e9411e29dcaac4ac69c
Abhay Vardhan <abhay@blip.me>
2014-01-07 08:42:34 -0800

    Fixed scales, mostly working

--- timeline.js
+++ timeline.js
@@ -1,25 +1,37 @@
-var margin = {top: 20, right: 50, bottom: 30, left: 50},
-  width = 960 - margin.left - margin.right,
+var margin = {top: 20, right: 10, bottom: 20, left: 10},
+  width = 200 - margin.left - margin.right,
   height = 500 - margin.top - margin.bottom;
 
-
+var Y = 'yellow';
+var O = 'orange';
+var G = 'green';
 
 var timelines = [{
-  dataset: ['red', 'green', 'blue']
+  dataset: [Y, O, G, O, O, O]
 }, {
-  dataset: ['red', 'green', 'blue']
+  dataset: [Y]
 }, {
-  dataset: ['red', 'green', 'blue']
+  dataset: [Y, O, G]
+}, {
+  dataset: [Y, O]
+}, {
+  dataset: [Y, O, G, O, O, O, G, O, O, O, O, G, O, O, O, G]
+}, {
+  dataset: [Y, O]
+}, {
+  dataset: [Y, O, G, O, O]
 }];
 
 var xScale = d3.scale.linear()
-  .domain([0, 10])
+  .domain([0, timelines.length])
   .range([0, width]);
+
+var maxSet = d3.max(timelines, function(e) {return e.dataset;});
 var yScale = d3.scale.linear()
-  .domain([0, 10])
+  .domain([0, maxSet.length])
   .range([0, height]);
 
-var svgAll = d3.select("body").append("svg")
+var svgAll = d3.select(".graph").append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
@@ -30,13 +42,13 @@ svgAll.selectAll('g')
   .enter()
   .append("g")
   .attr("transform", function (d, i) {
-    return "translate(" + i * 30 + ")";
+    return "translate(" + i * xScale(1) + ")";
   })
   .attr("r", function (d) {
     var svg = d3.select(this);
     var line = d3.svg.line()
       .x(function (d, i) {
-        return 10;
+        return xScale(0.5);
       })
       .y(function (d, i) {
         return yScale(d);
@@ -52,12 +64,12 @@ svgAll.selectAll('g')
         return d;
       })
       .attr("cx", function (d, i) {
-        return 10;
+        return xScale(0.5);
       })
       .attr("cy", function (d, i) {
         return yScale(i);
       })
       .attr("r", function (d) {
-        return 10;
+        return 6;
       });
   });
